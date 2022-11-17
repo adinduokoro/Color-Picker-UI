d8e8c9ff1b3b9d42f1bd640dfcdcb3ff85a3618e
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2022-11-17 13:18:20 -0500

    Finished
    A simple color picker UI with a toggle switch that converts the text from light to dark for easier readability.

--- src/Input.js
+++ src/Input.js
@@ -1,6 +1,27 @@
-const Input = () => {
+import colorNames from "colornames"
+
+const Input = ({ colorValue, setColorValue , setHexValue , isDarkText , setIsDarkText }) => {
   return (
-    <div>Input</div>
+    <form onSubmit={(e) => e.preventDefault()}>
+      <label>Add Color Name:</label>
+      <input
+        autoFocus
+        type="text"
+        placeholder="Add color name"
+        required
+        value={colorValue}
+        onChange={(e) => {
+          setColorValue(e.target.value)
+          setHexValue(colorNames(e.target.value))
+        }}
+      />
+      <button
+        type="button"
+        onClick={() => setIsDarkText(!isDarkText)}
+      >
+        Toggle Text Color
+      </button>
+    </form>
   )
 }
 
--- src/Square.js
+++ src/Square.js
@@ -1,9 +1,20 @@
-const Square = () => {
+const Square = ({ colorValue, hexValue , isDarkText }) => {
   return (
-    <div>
-      
-    </div>
+    <section
+      className='square'
+      style={{
+        backgroundColor: colorValue,
+        color: isDarkText ? "#000" : "#FFF"
+      }}
+    >
+      <p>{colorValue ? colorValue : "Empty Value"}</p>
+      <p>{hexValue ? hexValue : null}</p>
+    </section>
   )
+}
+
+Square.defaultProps = {
+  colorValue: "Empty Color Value"
 }
 
 export default Square
